55c848d73944a970cf5341868f847e9dda153967
roadaspen <709208680@qq.com>
2021-02-21 22:57:26 +0800

    add config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,8 @@
 module.exports = {
-    title: 'roadaspen‘s blog',
-    description: '永远行走在前进的路上'
+    title: 'roadaspen\'s blog',
+    description: '永远行走在前进的路上',
+    head: [
+        ['link', { rel: 'icon', href: '/logo.png' }]
+      ]
+    
 }
